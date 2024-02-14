ad14b8884d6d9d8079e11e9c0deb34264cccc3b1
yassinezay <yassine.zayane@esprit.tn>
2024-02-14 22:04:23 +0100

    intial commit

---
+++ src/main/java/edu/esprit/tests/Main.java
@@ -0,0 +1,12 @@
+package edu.esprit.tests;
+
+import edu.esprit.utils.DataSource;
+
+import javax.xml.crypto.Data;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSource ds= new DataSource();
+
+    }
+}
